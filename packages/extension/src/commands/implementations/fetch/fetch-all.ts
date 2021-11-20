3e42f33bec855808a19b9b7547b015b8d235d672
Jordan Mele <mele@canva.com>
2021-11-20 13:30:57 +1100

    Relocated fetch commands

--- packages/extension/src/commands/implementations/fetch/fetch-all.ts
+++ packages/extension/src/commands/implementations/fetch/fetch-all.ts
@@ -1,21 +1,21 @@
 import { window } from "vscode";
-import { ScmCommand } from "../../commands.js";
-import { Repository } from "../../repository.js";
-import { localize } from "../../util.js";
+import { ScmCommand } from "../../../commands.js";
+import { Repository } from "../../../repository.js";
+import { localize } from "../../../util.js";
 
 export function createCommand(): ScmCommand {
-	async function fetchPrune(repository: Repository): Promise<void> {
+	async function fetchAll(repository: Repository): Promise<void> {
 		if (repository.remotes.length === 0) {
 			window.showWarningMessage(localize('no remotes to fetch', "This repository has no remotes configured to fetch from."));
 			return;
 		}
 
-		await repository.fetchPrune();
+		await repository.fetchAll();
 	};
 
 	return {
-		commandId: 'git.fetchPrune',
-		method: fetchPrune,
+		commandId: 'git.fetchAll',
+		method: fetchAll,
 		options: {
 			repository: true,
 		},
--- packages/extension/src/commands/implementations/fetch/fetch-prune.ts
+++ packages/extension/src/commands/implementations/fetch/fetch-prune.ts
@@ -1,21 +1,21 @@
 import { window } from "vscode";
-import { ScmCommand } from "../../commands.js";
-import { Repository } from "../../repository.js";
-import { localize } from "../../util.js";
+import { ScmCommand } from "../../../commands.js";
+import { Repository } from "../../../repository.js";
+import { localize } from "../../../util.js";
 
 export function createCommand(): ScmCommand {
-	async function fetch(repository: Repository): Promise<void> {
+	async function fetchPrune(repository: Repository): Promise<void> {
 		if (repository.remotes.length === 0) {
 			window.showWarningMessage(localize('no remotes to fetch', "This repository has no remotes configured to fetch from."));
 			return;
 		}
 
-		await repository.fetchDefault();
+		await repository.fetchPrune();
 	};
 
 	return {
-		commandId: 'git.fetch',
-		method: fetch,
+		commandId: 'git.fetchPrune',
+		method: fetchPrune,
 		options: {
 			repository: true,
 		},
