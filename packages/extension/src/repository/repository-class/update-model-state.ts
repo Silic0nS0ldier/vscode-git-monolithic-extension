d4a5d34975b6519897479684b018ec01cf9fd425
Jordan Mele <mele@canva.com>
2022-03-08 22:12:59 +1100

    Don't block UI update on limit hit warning

--- packages/extension/src/repository/repository-class/update-model-state.ts
+++ packages/extension/src/repository/repository-class/update-model-state.ts
@@ -1,8 +1,9 @@
 import path from "node:path";
-import { commands, EventEmitter, Uri, window, workspace } from "vscode";
+import { commands, EventEmitter, Uri, window, workspace, WorkspaceConfiguration } from "vscode";
 import { Branch, Ref, Remote, Status } from "../../api/git.js";
 import { Repository } from "../../git.js";
 import { Commit } from "../../git/Commit.js";
+import { IFileStatus } from "../../git/IFileStatus.js";
 import { Submodule } from "../../git/Submodule.js";
 import { SourceControlUIGroup } from "../../ui/source-control.js";
 import { Box, localize } from "../../util.js";
@@ -32,6 +33,7 @@ export async function updateModelState(
     const scopedConfig = workspace.getConfiguration("git", Uri.file(repository.root));
     const ignoreSubmodules = scopedConfig.get<boolean>("ignoreSubmodules");
 
+    // TODO Account for potential missing items
     const { status, didHitLimit } = await repository.getStatus({ ignoreSubmodules });
 
     const config = workspace.getConfiguration("git");
@@ -40,38 +42,8 @@ export async function updateModelState(
     isRepositoryHuge.set(didHitLimit);
 
     if (didHitLimit && !shouldIgnore && !didWarnAboutLimit.get()) {
-        const knownHugeFolderPaths = await findKnownHugeFolderPathsToIgnore(repoRoot, run, repository);
-        const gitWarn = localize(
-            "huge",
-            "The git repository at '{0}' has too many active changes, only a subset of Git features will be enabled.",
-            repository.root,
-        );
-        const neverAgain = { title: localize("neveragain", "Don't Show Again") };
-
-        if (knownHugeFolderPaths.length > 0) {
-            const folderPath = knownHugeFolderPaths[0];
-            const folderName = path.basename(folderPath);
-
-            const addKnown = localize("add known", "Would you like to add '{0}' to .gitignore?", folderName);
-            const yes = { title: localize("yes", "Yes") };
-
-            const result = await window.showWarningMessage(`${gitWarn} ${addKnown}`, yes, neverAgain);
-
-            if (result === neverAgain) {
-                config.update("ignoreLimitWarning", true, false);
-                didWarnAboutLimit.set(true);
-            } else if (result === yes) {
-                ignore(run, repository, [Uri.file(folderPath)]);
-            }
-        } else {
-            const result = await window.showWarningMessage(gitWarn, neverAgain);
-
-            if (result === neverAgain) {
-                config.update("ignoreLimitWarning", true, false);
-            }
-
-            didWarnAboutLimit.set(true);
-        }
+        // Deliberately not awaited to keep model update going
+        handleLimitHit(repoRoot, run, repository, config, didWarnAboutLimit);
     }
 
     let newHEAD: Branch | undefined;
@@ -107,7 +79,6 @@ export async function updateModelState(
     submodules.set(newSubmodules);
     rebaseCommit.set(newRebaseCommit);
 
-    const untrackedChanges = scopedConfig.get<"mixed" | "separate" | "hidden">("untrackedChanges");
     const index: Resource[] = [];
     const workingTree: Resource[] = [];
     const merge: Resource[] = [];
@@ -131,192 +102,9 @@ export async function updateModelState(
         )
     }
 
-    status.forEach(raw => {
-        const uri = Uri.file(path.join(repository.root, raw.path));
-        const renameUri = raw.rename
-            ? Uri.file(path.join(repository.root, raw.rename))
-            : undefined;
-
-        function createResourceWithUri(
-            resourceGroupType: ResourceGroupType,
-            type: Status,
-            renameResourceUri?: Uri,
-        ) {
-            return createResource(
-                resourceGroupType,
-                uri,
-                type,
-                renameResourceUri,
-            )
-        }
-
-        switch (raw.x + raw.y) {
-            case "??":
-                switch (untrackedChanges) {
-                    case "mixed":
-                        return workingTree.push(
-                            createResourceWithUri(
-                                ResourceGroupType.WorkingTree,
-                                Status.UNTRACKED,
-                            ),
-                        );
-                    case "separate":
-                        return untracked.push(
-                            createResourceWithUri(
-                                ResourceGroupType.Untracked,
-                                Status.UNTRACKED,
-                            ),
-                        );
-                    default:
-                        return undefined;
-                }
-            case "!!":
-                switch (untrackedChanges) {
-                    case "mixed":
-                        return workingTree.push(
-                            createResourceWithUri(
-                                ResourceGroupType.WorkingTree,
-                                Status.IGNORED,
-                            ),
-                        );
-                    case "separate":
-                        return untracked.push(
-                            createResourceWithUri(
-                                ResourceGroupType.Untracked,
-                                Status.IGNORED,
-                            ),
-                        );
-                    default:
-                        return undefined;
-                }
-            case "DD":
-                return merge.push(
-                    createResourceWithUri(
-                        ResourceGroupType.Merge,
-                        Status.BOTH_DELETED,
-                    ),
-                );
-            case "AU":
-                return merge.push(
-                    createResourceWithUri(
-                        ResourceGroupType.Merge,
-                        Status.ADDED_BY_US,
-                    ),
-                );
-            case "UD":
-                return merge.push(
-                    createResourceWithUri(
-                        ResourceGroupType.Merge,
-                        Status.DELETED_BY_THEM,
-                    ),
-                );
-            case "UA":
-                return merge.push(
-                    createResourceWithUri(
-                        ResourceGroupType.Merge,
-                        Status.ADDED_BY_THEM,
-                    ),
-                );
-            case "DU":
-                return merge.push(
-                    createResourceWithUri(
-                        ResourceGroupType.Merge,
-                        Status.DELETED_BY_US,
-                    ),
-                );
-            case "AA":
-                return merge.push(
-                    createResourceWithUri(
-                        ResourceGroupType.Merge,
-                        Status.BOTH_ADDED,
-                    ),
-                );
-            case "UU":
-                return merge.push(
-                    createResourceWithUri(
-                        ResourceGroupType.Merge,
-                        Status.BOTH_MODIFIED,
-                    ),
-                );
-        }
-
-        switch (raw.x) {
-            case "M":
-                index.push(
-                    createResourceWithUri(
-                        ResourceGroupType.Index,
-                        Status.INDEX_MODIFIED,
-                    ),
-                );
-                break;
-            case "A":
-                index.push(
-                    createResourceWithUri(
-                        ResourceGroupType.Index,
-                        Status.INDEX_ADDED,
-                    ),
-                );
-                break;
-            case "D":
-                index.push(
-                    createResourceWithUri(
-                        ResourceGroupType.Index,
-                        Status.INDEX_DELETED,
-                    ),
-                );
-                break;
-            case "R":
-                index.push(
-                    createResourceWithUri(
-                        ResourceGroupType.Index,
-                        Status.INDEX_RENAMED,
-                        renameUri,
-                    ),
-                );
-                break;
-            case "C":
-                index.push(
-                    createResourceWithUri(
-                        ResourceGroupType.Index,
-                        Status.INDEX_COPIED,
-                        renameUri,
-                    ),
-                );
-                break;
-        }
-
-        switch (raw.y) {
-            case "M":
-                workingTree.push(
-                    createResourceWithUri(
-                        ResourceGroupType.WorkingTree,
-                        Status.MODIFIED,
-                        renameUri,
-                    ),
-                );
-                break;
-            case "D":
-                workingTree.push(
-                    createResourceWithUri(
-                        ResourceGroupType.WorkingTree,
-                        Status.DELETED,
-                        renameUri,
-                    ),
-                );
-                break;
-            case "A":
-                workingTree.push(
-                    createResourceWithUri(
-                        ResourceGroupType.WorkingTree,
-                        Status.INTENT_TO_ADD,
-                        renameUri,
-                    ),
-                );
-                break;
-        }
-
-        return undefined;
-    });
+    for (const fileStatus of status) {
+        pigeonholeFileStatus(repoRoot, fileStatus, createResource, index, workingTree, merge, untracked);
+    }
 
     // set resource groups
     sourceControlUI.mergeGroup.resourceStates = merge;
@@ -337,4 +125,221 @@ export async function updateModelState(
     onDidChangeStatusEmitter.fire();
 
     sourceControlUI.sourceControl.commitTemplate = await getInputTemplate(repository);
+}
+
+function pigeonholeFileStatus(
+    repoRoot: string,
+    fileStatus: IFileStatus,
+    createResource: (resourceGroupType: ResourceGroupType,
+        resourceUri: Uri,
+        type: Status,
+        renameResourceUri?: Uri,) => Resource,
+    index: Resource[],
+    workingTree: Resource[],
+    merge: Resource[],
+    untracked: Resource[],
+
+) {
+    const uri = Uri.file(path.join(repoRoot, fileStatus.path));
+    const renameUri = fileStatus.rename
+        ? Uri.file(path.join(repoRoot, fileStatus.rename))
+        : undefined;
+
+    function createResourceWithUri(
+        resourceGroupType: ResourceGroupType,
+        type: Status,
+        renameResourceUri?: Uri,
+    ) {
+        return createResource(
+            resourceGroupType,
+            uri,
+            type,
+            renameResourceUri,
+        )
+    }
+
+    switch (fileStatus.x + fileStatus.y) {
+        case "??":
+            return untracked.push(
+                createResourceWithUri(
+                    ResourceGroupType.Untracked,
+                    Status.UNTRACKED,
+                ),
+            );
+        case "!!":
+            return untracked.push(
+                createResourceWithUri(
+                    ResourceGroupType.Untracked,
+                    Status.IGNORED,
+                ),
+            );
+        case "DD":
+            return merge.push(
+                createResourceWithUri(
+                    ResourceGroupType.Merge,
+                    Status.BOTH_DELETED,
+                ),
+            );
+        case "AU":
+            return merge.push(
+                createResourceWithUri(
+                    ResourceGroupType.Merge,
+                    Status.ADDED_BY_US,
+                ),
+            );
+        case "UD":
+            return merge.push(
+                createResourceWithUri(
+                    ResourceGroupType.Merge,
+                    Status.DELETED_BY_THEM,
+                ),
+            );
+        case "UA":
+            return merge.push(
+                createResourceWithUri(
+                    ResourceGroupType.Merge,
+                    Status.ADDED_BY_THEM,
+                ),
+            );
+        case "DU":
+            return merge.push(
+                createResourceWithUri(
+                    ResourceGroupType.Merge,
+                    Status.DELETED_BY_US,
+                ),
+            );
+        case "AA":
+            return merge.push(
+                createResourceWithUri(
+                    ResourceGroupType.Merge,
+                    Status.BOTH_ADDED,
+                ),
+            );
+        case "UU":
+            return merge.push(
+                createResourceWithUri(
+                    ResourceGroupType.Merge,
+                    Status.BOTH_MODIFIED,
+                ),
+            );
+    }
+
+    switch (fileStatus.x) {
+        case "M":
+            index.push(
+                createResourceWithUri(
+                    ResourceGroupType.Index,
+                    Status.INDEX_MODIFIED,
+                ),
+            );
+            break;
+        case "A":
+            index.push(
+                createResourceWithUri(
+                    ResourceGroupType.Index,
+                    Status.INDEX_ADDED,
+                ),
+            );
+            break;
+        case "D":
+            index.push(
+                createResourceWithUri(
+                    ResourceGroupType.Index,
+                    Status.INDEX_DELETED,
+                ),
+            );
+            break;
+        case "R":
+            index.push(
+                createResourceWithUri(
+                    ResourceGroupType.Index,
+                    Status.INDEX_RENAMED,
+                    renameUri,
+                ),
+            );
+            break;
+        case "C":
+            index.push(
+                createResourceWithUri(
+                    ResourceGroupType.Index,
+                    Status.INDEX_COPIED,
+                    renameUri,
+                ),
+            );
+            break;
+    }
+
+    switch (fileStatus.y) {
+        case "M":
+            workingTree.push(
+                createResourceWithUri(
+                    ResourceGroupType.WorkingTree,
+                    Status.MODIFIED,
+                    renameUri,
+                ),
+            );
+            break;
+        case "D":
+            workingTree.push(
+                createResourceWithUri(
+                    ResourceGroupType.WorkingTree,
+                    Status.DELETED,
+                    renameUri,
+                ),
+            );
+            break;
+        case "A":
+            workingTree.push(
+                createResourceWithUri(
+                    ResourceGroupType.WorkingTree,
+                    Status.INTENT_TO_ADD,
+                    renameUri,
+                ),
+            );
+            break;
+    }
+
+    return undefined;
+}
+
+async function handleLimitHit(
+    repoRoot: string,
+    run: RunFn<void> & RunFn<Set<string>>,
+    repository: Repository,
+    config: WorkspaceConfiguration,
+    didWarnAboutLimit: Box<boolean>,
+) {
+    const knownHugeFolderPaths = await findKnownHugeFolderPathsToIgnore(repoRoot, run, repository);
+    const gitWarn = localize(
+        "huge",
+        "The git repository at '{0}' has too many active changes, only a subset of Git features will be enabled.",
+        repoRoot,
+    );
+    const neverAgain = { title: localize("neveragain", "Don't Show Again") };
+
+    if (knownHugeFolderPaths.length > 0) {
+        const folderPath = knownHugeFolderPaths[0];
+        const folderName = path.basename(folderPath);
+
+        const addKnown = localize("add known", "Would you like to add '{0}' to .gitignore?", folderName);
+        const yes = { title: localize("yes", "Yes") };
+
+        // this is why things slow down
+        const result = await window.showWarningMessage(`${gitWarn} ${addKnown}`, yes, neverAgain);
+
+        if (result === neverAgain) {
+            config.update("ignoreLimitWarning", true, false);
+            didWarnAboutLimit.set(true);
+        } else if (result === yes) {
+            ignore(run, repository, [Uri.file(folderPath)]);
+        }
+    } else {
+        const result = await window.showWarningMessage(gitWarn, neverAgain);
+
+        if (result === neverAgain) {
+            config.update("ignoreLimitWarning", true, false);
+        }
+
+        didWarnAboutLimit.set(true);
+    }
 }
