86d35ae27c4bd71443f6d91bd1f7f70901a0a393
Jordan Mele <mele@canva.com>
2022-04-25 14:35:30 +1000

    Fixed grammer updater

--- packages/build-tools/update-grammars.js
+++ packages/build-tools/update-grammars.js
@@ -3,16 +3,27 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import * as path from "node:path";
 import updateGrammar from "vscode-grammar-updater";
+import { extensionPkg } from "./util/paths.js";
+
+// vscode-grammer-updater assumes it is running in target package
+process.chdir(extensionPkg);
 
 updateGrammar.update(
     "textmate/git.tmbundle",
     "Syntaxes/Git%20Commit%20Message.tmLanguage",
-    "./syntaxes/git-commit.tmLanguage.json",
+    path.join(extensionPkg, "syntaxes/git-commit.tmLanguage.json"),
 );
 updateGrammar.update(
     "textmate/git.tmbundle",
     "Syntaxes/Git%20Rebase%20Message.tmLanguage",
-    "./syntaxes/git-rebase.tmLanguage.json",
+    path.join(extensionPkg, "syntaxes/git-rebase.tmLanguage.json"),
 );
-updateGrammar.update("textmate/diff.tmbundle", "Syntaxes/Diff.plist", "./syntaxes/diff.tmLanguage.json");
+updateGrammar.update(
+    "textmate/diff.tmbundle",
+    "Syntaxes/Diff.plist",
+    path.join(extensionPkg, "syntaxes/diff.tmLanguage.json"),
+);
+
+console.log("NOTE It is normal for this to exit with a cannot get version error.");
