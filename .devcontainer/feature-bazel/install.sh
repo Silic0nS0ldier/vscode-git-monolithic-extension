b5f18848b9368ad00392c65f3a8f1fb1bf0d1471
Jordan Mele <jordan.mele@outlook.com.au>
2024-11-02 02:20:34 +0000

    Bump pnpm to latest

--- .devcontainer/feature-bazel/install.sh
+++ .devcontainer/feature-bazel/install.sh
@@ -11,14 +11,14 @@ if [[ $ARCH == "arm64" ]]; then
     curl https://github.com/bazelbuild/buildtools/releases/download/v7.1.2/buildifier-linux-arm64 -Lo /usr/local/bin/buildifier
     curl https://github.com/bazelbuild/buildtools/releases/download/v7.1.2/buildozer-linux-arm64 -Lo /usr/local/bin/buildozer
     curl https://github.com/bazelbuild/buildtools/releases/download/v7.1.2/unused_deps-linux-arm64 -Lo /usr/local/bin/unused_deps
-    curl https://github.com/pnpm/pnpm/releases/download/v9.7.0/pnpm-linux-arm64 -Lo /usr/local/bin/pnpm
+    curl https://github.com/pnpm/pnpm/releases/download/v9.12.3/pnpm-linux-arm64 -Lo /usr/local/bin/pnpm
 elif [[ $ARCH == "x86_64" ]]; then
     curl https://github.com/bazelbuild/bazelisk/releases/download/v1.20.0/bazelisk-linux-amd64 -Lo /usr/local/bin/bazel
     curl https://github.com/bazelbuild/buildtools/releases/download/v7.1.2/buildifier-linux-amd64 -Lo /usr/local/bin/buildifier
     curl https://github.com/bazelbuild/buildtools/releases/download/v7.1.2/buildozer-linux-amd64 -Lo /usr/local/bin/buildozer
     curl https://github.com/bazelbuild/buildtools/releases/download/v7.1.2/unused_deps-linux-amd64 -Lo /usr/local/bin/unused_deps
     curl https://github.com/withered-magic/starpls/releases/download/v0.1.14/starpls-linux-amd64 -Lo /usr/local/bin/starpls
-    curl https://github.com/pnpm/pnpm/releases/download/v9.7.0/pnpm-linux-x64 -Lo /usr/local/bin/pnpm
+    curl https://github.com/pnpm/pnpm/releases/download/v9.12.3/pnpm-linux-x64 -Lo /usr/local/bin/pnpm
 
     chmod +x /usr/local/bin/starpls ;
 else
