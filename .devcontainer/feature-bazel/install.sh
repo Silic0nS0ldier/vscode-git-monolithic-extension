47fdb2b6111d020b4fa864a657ce95fb136534df
Jordan <SiliconSoldier@outlook.com.au>
2025-06-18 20:08:36 +1000

    Mass update (#154)
    - `bazel` 8.2.1 -> 8.3.0rc3
    - `buildtools` 8.2.0 -> 8.2.1
    - `pnpm` 10.11.0 -> 10.12.1
    - `aspect_bazel_lib` 2.17.1 -> 2.19.4
    - `rules_shell` 0.4.1 -> 0.5.0
    - `node` 24.1.0 -> 24.2.0
    - `@rollup/plugin-commonjs` 28.0.5 -> 28.0.6
    - `typescript-eslint` 8.34.0 -> 8.34.1
    - `@types/node` (default) 22.15.21 -> 24.0.3
    - `@types/node` (vscode) 20.17.50 -> 20.19.1

--- .devcontainer/feature-bazel/install.sh
+++ .devcontainer/feature-bazel/install.sh
@@ -9,19 +9,19 @@ ARCH="$(uname --processor)"
 
 if [[ $ARCH == "arm64" ]] || [[ $ARCH == "aarch64" ]]; then
     curl https://github.com/bazelbuild/bazelisk/releases/download/v1.26.0/bazelisk-linux-arm64 -Lo /usr/local/bin/bazel
-    curl https://github.com/bazelbuild/buildtools/releases/download/v8.2.0/buildifier-linux-arm64 -Lo /usr/local/bin/buildifier
-    curl https://github.com/bazelbuild/buildtools/releases/download/v8.2.0/buildozer-linux-arm64 -Lo /usr/local/bin/buildozer
-    curl https://github.com/bazelbuild/buildtools/releases/download/v8.2.0/unused_deps-linux-arm64 -Lo /usr/local/bin/unused_deps
+    curl https://github.com/bazelbuild/buildtools/releases/download/v8.2.1/buildifier-linux-arm64 -Lo /usr/local/bin/buildifier
+    curl https://github.com/bazelbuild/buildtools/releases/download/v8.2.1/buildozer-linux-arm64 -Lo /usr/local/bin/buildozer
+    curl https://github.com/bazelbuild/buildtools/releases/download/v8.2.1/unused_deps-linux-arm64 -Lo /usr/local/bin/unused_deps
     curl https://github.com/withered-magic/starpls/releases/download/v0.1.21/starpls-linux-aarch64 -Lo /usr/local/bin/starpls
-    curl https://github.com/pnpm/pnpm/releases/download/v10.11.0/pnpm-linux-arm64 -Lo /usr/local/bin/pnpm
+    curl https://github.com/pnpm/pnpm/releases/download/v10.12.1/pnpm-linux-arm64 -Lo /usr/local/bin/pnpm
 
 elif [[ $ARCH == "x86_64" ]]; then
     curl https://github.com/bazelbuild/bazelisk/releases/download/v1.26.0/bazelisk-linux-amd64 -Lo /usr/local/bin/bazel
-    curl https://github.com/bazelbuild/buildtools/releases/download/v8.2.0/buildifier-linux-amd64 -Lo /usr/local/bin/buildifier
-    curl https://github.com/bazelbuild/buildtools/releases/download/v8.2.0/buildozer-linux-amd64 -Lo /usr/local/bin/buildozer
-    curl https://github.com/bazelbuild/buildtools/releases/download/v8.2.0/unused_deps-linux-amd64 -Lo /usr/local/bin/unused_deps
+    curl https://github.com/bazelbuild/buildtools/releases/download/v8.2.1/buildifier-linux-amd64 -Lo /usr/local/bin/buildifier
+    curl https://github.com/bazelbuild/buildtools/releases/download/v8.2.1/buildozer-linux-amd64 -Lo /usr/local/bin/buildozer
+    curl https://github.com/bazelbuild/buildtools/releases/download/v8.2.1/unused_deps-linux-amd64 -Lo /usr/local/bin/unused_deps
     curl https://github.com/withered-magic/starpls/releases/download/v0.1.21/starpls-linux-amd64 -Lo /usr/local/bin/starpls
-    curl https://github.com/pnpm/pnpm/releases/download/v10.11.0/pnpm-linux-x64 -Lo /usr/local/bin/pnpm
+    curl https://github.com/pnpm/pnpm/releases/download/v10.12.1/pnpm-linux-x64 -Lo /usr/local/bin/pnpm
 else
     echo "Unknown arch $ARCH"
     exit 1
