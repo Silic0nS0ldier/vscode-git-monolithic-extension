85dd9a706ecee67e6d1996f9e29f661193ed323f
Jordan Mele <jordan.mele@outlook.com.au>
2025-06-22 23:26:01 +1000

    Fix ref, disable apt layer (corruption?)

--- .devcontainer/devcontainer-check.mjs
+++ .devcontainer/devcontainer-check.mjs
@@ -4,10 +4,9 @@ let str = await fs.readFile("./devcontainer.json", "utf-8");
 const devcontainerJson = JSON.parse(str);
 const actual = devcontainerJson.image;
 
-str = await fs.readFile("./multi_platform.json.sha256", "utf-8");
 const expectedImage = "ghcr.io/silic0ns0ldier/vscode-git-monolithic-extension/devcontainer";
-const expectedSha256 = str.split(":")[1].trim();
-const expected = `${expectedImage}:${expectedSha256}`;
+const expectedSha256 = (await fs.readFile("./multi_platform.json.sha256", "utf-8")).trim();
+const expected = `${expectedImage}@${expectedSha256}`;
 
 if (actual !== expected) {
     console.error(`Actual  : ${actual}`);
