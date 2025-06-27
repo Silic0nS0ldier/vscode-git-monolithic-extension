a2920508af30b5ca3b9c16e8b4bab7b39c08b5f8
Jordan <SiliconSoldier@outlook.com.au>
2025-06-27 21:46:37 +1000

    Mass update (#158)

--- .devcontainer/feature-bazel/install.sh
+++ .devcontainer/feature-bazel/install.sh
@@ -13,7 +13,7 @@ if [[ $ARCH == "arm64" ]] || [[ $ARCH == "aarch64" ]]; then
     curl https://github.com/bazelbuild/buildtools/releases/download/v8.2.1/buildozer-linux-arm64 -Lo /usr/local/bin/buildozer
     curl https://github.com/bazelbuild/buildtools/releases/download/v8.2.1/unused_deps-linux-arm64 -Lo /usr/local/bin/unused_deps
     curl https://github.com/withered-magic/starpls/releases/download/v0.1.21/starpls-linux-aarch64 -Lo /usr/local/bin/starpls
-    curl https://github.com/pnpm/pnpm/releases/download/v10.12.1/pnpm-linux-arm64 -Lo /usr/local/bin/pnpm
+    curl https://github.com/pnpm/pnpm/releases/download/v10.12.4/pnpm-linux-arm64 -Lo /usr/local/bin/pnpm
 
 elif [[ $ARCH == "x86_64" ]]; then
     curl https://github.com/bazelbuild/bazelisk/releases/download/v1.26.0/bazelisk-linux-amd64 -Lo /usr/local/bin/bazel
@@ -21,7 +21,7 @@ elif [[ $ARCH == "x86_64" ]]; then
     curl https://github.com/bazelbuild/buildtools/releases/download/v8.2.1/buildozer-linux-amd64 -Lo /usr/local/bin/buildozer
     curl https://github.com/bazelbuild/buildtools/releases/download/v8.2.1/unused_deps-linux-amd64 -Lo /usr/local/bin/unused_deps
     curl https://github.com/withered-magic/starpls/releases/download/v0.1.21/starpls-linux-amd64 -Lo /usr/local/bin/starpls
-    curl https://github.com/pnpm/pnpm/releases/download/v10.12.1/pnpm-linux-x64 -Lo /usr/local/bin/pnpm
+    curl https://github.com/pnpm/pnpm/releases/download/v10.12.4/pnpm-linux-x64 -Lo /usr/local/bin/pnpm
 else
     echo "Unknown arch $ARCH"
     exit 1
