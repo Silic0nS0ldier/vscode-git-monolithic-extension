dfd143a3f16a5fb10875c65c2ca386f4866aa452
Jordan Mele <jordan.mele@outlook.com.au>
2025-07-16 11:02:53 +0000

    finish wiring

--- extension/src/ui/inlay-hints-provider.ts
+++ extension/src/ui/inlay-hints-provider.ts
@@ -1,14 +1,28 @@
-import { type InlayHintsProvider, type InlayHint, Position } from "vscode";
-import { hasExecutableBit } from "monolithic-git-interop/api/ls-tree/has-executable-bit";
+import { type InlayHintsProvider, Position, Range, workspace, EventEmitter } from "vscode";
 import { fromGitUri, isGitUri } from "../uri.js";
+import { inlayHintsForFilePermissions } from "../util/config.js";
+import type { Model } from "../model.js";
 
 const START_POSITION = new Position(0, 0);
+const SHEBANG_RANGE = new Range(0, 0, 0, 2);
 
-export function createInlayHintsProvider(): InlayHintsProvider {
+export function createInlayHintsProvider(model: Model): InlayHintsProvider {
+    let inlayFilePermissions = inlayHintsForFilePermissions();
+    const onConfigurationChangedEmitter = new EventEmitter<void>();
+    workspace.onDidChangeConfiguration(event => {
+        if (event.affectsConfiguration("git-monolithic.inlayHints.filePermissions")) {
+            inlayFilePermissions = inlayHintsForFilePermissions();
+            onConfigurationChangedEmitter.fire();
+        }
+    });
+
     return {
-        // TODO Wire up for configuration (disabling inlay hint)
-        onDidChangeInlayHints: undefined,
-        async provideInlayHints(document, range, token) {
+        onDidChangeInlayHints: onConfigurationChangedEmitter.event,
+        async provideInlayHints(document, range, _token) {
+            if (!inlayFilePermissions) {
+                return [];
+            }
+
             if (!range.start.isEqual(START_POSITION)) {
                 // Only support execbit inlay hint for now, which is always at the start of the document
                 return [];
@@ -28,19 +42,41 @@ export function createInlayHintsProvider(): InlayHintsProvider {
                 }
             })();
 
-            const r = await hasExecutableBit(null, null, filePath, ref);
+            const repository = model.getRepository(filePath);
+            if (!repository) {
+                return [];
+            }
+            
+            const isExecutable = await repository.fileHasExecutableBit(filePath, ref);
+            const hasShebang = document.getText(SHEBANG_RANGE) === "#!";
 
-            const hint: InlayHint = {
-                label: "Example Hint",
-                position: START_POSITION,
-                paddingRight: true,
-                tooltip: "This is an example inlay hint",
-            };
-            return [hint];
+            if (hasShebang && !isExecutable) {
+                // If the file has a shebang but is not executable.
+                // This is usually a mistake, so we provide a hint.
+                return [{
+                    label: "-e",
+                    position: START_POSITION,
+                    paddingRight: true,
+                    tooltip: "This file has a shebang but is not marked as executable.",
+                }];
+            }
+
+            if (isExecutable && !hasShebang) {
+                // If the file is executable and does not have a shebang.
+                // This is unusual, so we provide a hint.
+                return [{
+                    label: "+x",
+                    position: START_POSITION,
+                    paddingRight: true,
+                    tooltip: "This file is marked as executable.",
+                }];
+            }
+
+            return [];
         },
-        resolveInlayHint(hint, token) {
-            // Example implementation: return the hint as is
-            return hint;
-        }
+        // resolveInlayHint(hint, token) {
+        //     // Example implementation: return the hint as is
+        //     return hint;
+        // }
     };
 }
