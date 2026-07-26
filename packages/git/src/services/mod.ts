85c0b501cb563e712a1b55d2a452cee9c313ea2d
Jordan Mele <jordan.mele@outlook.com.au>
2026-07-26 11:16:16 +0000

    Deduplicate required service declarations

--- packages/git/src/services/mod.ts
+++ packages/git/src/services/mod.ts
@@ -1,20 +1,43 @@
 import type { LogFn, SpawnFn } from "../cli/create.js";
 
-export type AllServices = {
+export type FsService = {
     fs: {
         exists: (path: string) => boolean;
     };
+};
+
+export type ShellService = {
     shell: {
         which: (cmd: string, options: { path: string; pathExt?: string, nothrow: true }) => Promise<string|null>;
     };
+};
+
+export type ChildProcessService = {
     child_process: {
         spawn: SpawnFn;
     };
+};
+
+export type ProcessService = {
     process: {
         env: NodeJS.ProcessEnv;
     };
+};
+
+export type OsService = {
     os: {
         platform: string;
     };
+};
+
+export type LogService = {
     log?: LogFn,
 };
+
+export type AllServices =
+    & FsService
+    & ShellService
+    & ChildProcessService
+    & ProcessService
+    & OsService
+    & LogService;
