200e81fbcf23f72e08425db006b4b5cee1a533bb
Jordan Mele <mele@canva.com>
2026-07-28 20:04:55 +1000

    Add loop guard to SCM state

--- extension/src/repository/repository-class/update-model-state.ts
+++ extension/src/repository/repository-class/update-model-state.ts
@@ -13,6 +13,72 @@ import { getInputTemplate } from "./get-input-template.js";
 import { getRebaseCommit } from "./get-rebase-commit.js";
 import { pigeonholeFileStatus } from "./update-model-state/pigeonhole-file-status.js";
 
+function arraysEqualBy<T>(a: readonly T[], b: readonly T[], eq: (x: T, y: T) => boolean): boolean {
+    if (a === b) {
+        return true;
+    }
+    if (a.length !== b.length) {
+        return false;
+    }
+    for (let i = 0; i < a.length; i++) {
+        if (!eq(a[i], b[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function branchEqual(a: Branch | undefined, b: Branch | undefined): boolean {
+    if (a === b) {
+        return true;
+    }
+    if (!a || !b) {
+        return false;
+    }
+    return a.type === b.type
+        && a.name === b.name
+        && a.commit === b.commit
+        && a.remote === b.remote
+        && a.ahead === b.ahead
+        && a.behind === b.behind
+        && (a.upstream === b.upstream
+            || (!!a.upstream && !!b.upstream
+                && a.upstream.name === b.upstream.name
+                && a.upstream.remote === b.upstream.remote));
+}
+
+function refEqual(a: Ref, b: Ref): boolean {
+    return a.type === b.type && a.name === b.name && a.commit === b.commit && a.remote === b.remote;
+}
+
+function remoteEqual(a: Remote, b: Remote): boolean {
+    return a.name === b.name
+        && a.fetchUrl === b.fetchUrl
+        && a.pushUrl === b.pushUrl
+        && a.isReadOnly === b.isReadOnly;
+}
+
+function submoduleEqual(a: Submodule, b: Submodule): boolean {
+    return a.name === b.name && a.path === b.path && a.url === b.url;
+}
+
+function rebaseCommitEqual(a: Commit | undefined, b: Commit | undefined): boolean {
+    if (a === b) {
+        return true;
+    }
+    if (!a || !b) {
+        return false;
+    }
+    // The hash identifies the commit; other fields are derived from it.
+    return a.hash === b.hash;
+}
+
+function resourceEqual(a: Resource, b: Resource): boolean {
+    return a.state.type === b.state.type
+        && a.state.resourceUri.fsPath === b.state.resourceUri.fsPath
+        && a.state.renameResourceUri?.fsPath === b.state.renameResourceUri?.fsPath;
+}
+
 export async function updateModelState(
     repository: Repository,
     HEAD: Box<Branch | undefined>,
@@ -26,6 +92,19 @@ export async function updateModelState(
     sourceControlUI: SourceControlUIGroup,
 ): Promise<void> {
     const ignoreSubmodules = config.ignoreSubmodules(Uri.file(repository.root));
+
+    // Snapshot previous state so we can detect whether this refresh actually
+    // produced any change worth notifying listeners about. Reading now (before
+    // any `.set()` below) captures the pre-update values.
+    const prevHEAD = HEAD.get();
+    const prevRefs = refs.get();
+    const prevRemotes = remotes.get();
+    const prevSubmodules = submodules.get();
+    const prevRebaseCommit = rebaseCommit.get();
+    const prevMerge = sourceControlUI.mergeGroup.resourceStates.get();
+    const prevIndex = sourceControlUI.stagedGroup.resourceStates.get();
+    const prevTracked = sourceControlUI.trackedGroup.resourceStates.get();
+    const prevUntracked = sourceControlUI.untrackedGroup.resourceStates.get();
 
     // TODO Account for potential missing items when limit is hit
     // Could use placeholder like "(empty)"
@@ -124,7 +203,19 @@ export async function updateModelState(
         [...merge, ...index, ...tracked, ...untracked].map(r => r.state.resourceUri.fsPath.toString()),
     );
 
-    onDidChangeStatusEmitter.fire();
+    const hasMeaningfulChange = !branchEqual(prevHEAD, newHEAD)
+        || !arraysEqualBy(prevRefs, newRefs, refEqual)
+        || !arraysEqualBy(prevRemotes, newRemotes, remoteEqual)
+        || !arraysEqualBy(prevSubmodules, newSubmodules, submoduleEqual)
+        || !rebaseCommitEqual(prevRebaseCommit, newRebaseCommit)
+        || !arraysEqualBy(prevMerge, merge, resourceEqual)
+        || !arraysEqualBy(prevIndex, index, resourceEqual)
+        || !arraysEqualBy(prevTracked, tracked, resourceEqual)
+        || !arraysEqualBy(prevUntracked, untracked, resourceEqual);
+
+    if (hasMeaningfulChange) {
+        onDidChangeStatusEmitter.fire();
+    }
 
     sourceControlUI.sourceControl.commitTemplate = await getInputTemplate(repository);
 }
